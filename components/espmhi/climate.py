9ddf6e6ed9aa13d24a2e3f4236575c6224f30fa6
Kelvin Chan <kelvin.chanwh@hotmail.com>
2025-06-09 02:31:10 +0800

    Add class

--- components/espmhi/climate.py
+++ components/espmhi/climate.py
@@ -41,7 +41,7 @@ FAN_VALUES = {
     "high": FanValues.ESPMHI_FAN3,
 }
 
-CONFIG_SCHEMA = climate_ir.climate_ir_schema.extend(
+CONFIG_SCHEMA = climate_ir.climate_ir_schema(espmhiClimate).extend(
     {
         cv.GenerateID(): cv.declare_id(espmhiClimate),
         cv.Required(CONF_HORIZONTAL_DEFAULT): cv.enum(HORIZONTAL_DIRECTIONS),
