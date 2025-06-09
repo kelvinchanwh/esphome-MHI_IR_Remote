8ada3086eeba2f7a5e433190b4841cb9c08af5ce
Kelvin Chan <kelvin.chanwh@hotmail.com>
2025-06-09 11:06:35 +0800

    Use new_climate_ir

--- components/espmhi/climate.py
+++ components/espmhi/climate.py
@@ -52,8 +52,8 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await climate_ir.register_climate_ir(var, config)
+    # var = cg.new_Pvariable(config[CONF_ID])
+    var = await climate_ir.new_climate_ir(config)
     cg.add(var.set_horizontal_default(config[CONF_HORIZONTAL_DEFAULT]))
     cg.add(var.set_vertical_default(config[CONF_VERTICAL_DEFAULT]))
     cg.add(var.set_fan_default(config[CONF_FAN_DEFAULT]))
